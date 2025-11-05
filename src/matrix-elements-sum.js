48da52ef761e40615e3271655d57a7ac3958d672
nastya3323 <gomes-96@mail.ru>
2025-11-05 20:18:44 +0400

    feat: implement getMatrixElementsSum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,11 +16,28 @@ const { NotImplementedError } = require('../lib');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getMatrixElementsSum(matrix) {
+  let sum = 0;
+
+  for (let i = 0; i < matrix[0].length; i += 1) {
+    let isZero = false;
+
+    for (let j = 0; j < matrix.length; j += 1) {
+      if (!isZero) {
+        sum += matrix[j][i];
+      }
+
+      if (matrix[j][i] === 0) {
+        isZero = true;
+      } else {
+        isZero = false;
+      }
+    }
+  }
+
+  return sum;
 }
 
 module.exports = {
-  getMatrixElementsSum
+  getMatrixElementsSum,
 };
