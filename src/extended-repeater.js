f448f90a509ed61db195a2fe71ab36050da011cf
nastya3323 <gomes-96@mail.ru>
2025-11-10 18:55:05 +0400

    feat: implement repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -16,9 +16,47 @@ const { NotImplementedError } = require('../lib');
  *
  */
 
-function repeater(/* str, options */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function repeater(str, options) {
+  let {
+    repeatTimes = 1,
+    separator = '+',
+    addition,
+    additionRepeatTimes,
+    additionSeparator = '|',
+  } = options;
+
+  if (addition && !additionRepeatTimes) {
+    additionRepeatTimes = 1;
+  }
+
+  let result = '';
+
+  let intermediateStr = String(str);
+
+  let j = 0;
+
+  while (j < additionRepeatTimes) {
+    intermediateStr += addition;
+
+    if (j !== additionRepeatTimes - 1) {
+      intermediateStr += additionSeparator;
+    }
+
+    j += 1;
+  }
+
+  let i = 0;
+
+  while (i < repeatTimes) {
+    result += intermediateStr;
+
+    if (i !== repeatTimes - 1) {
+      result += separator;
+    }
+    i += 1;
+  }
+
+  return result;
 }
 
 module.exports = {
